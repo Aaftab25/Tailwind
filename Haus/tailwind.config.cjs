d8a5a72b7545bf6c52bb30b50d9ad37fc1622c32
Aaftab <shaikhaaftab2510@gmail.com>
2023-02-25 18:25:02 +0530

    add new

---
+++ Haus/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      color: {
+        gray: '#cdd2a5',
+      },
+    },
+  },
+  plugins: [],
+}

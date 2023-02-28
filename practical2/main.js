78a94c19fb0f39a68a46cf467cfe5d1f0d09f4f6
Aaftab <shaikhaaftab2510@gmail.com>
2023-02-28 06:52:57 +0530

    final

--- practical2/main.js
+++ practical2/main.js
@@ -1,11 +1,2 @@
 import './style.css'
-const burger = document.querySelector('#burger');
-const menu = document.querySelector('#menu');
 
-burger.addEventListener('click', () => {
-    if(menu.classList.contains('hidden')){
-        menu.classList.remove('hidden');
-    }else{
-        menu.classList.add('hidden');
-    }
-})
